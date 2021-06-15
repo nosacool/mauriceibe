95307cff23f9b0e9b844f031ecb918c4bbe729cf
nosacool <nosacool2007@yahoo.com>
2021-06-15 19:22:43 +0100

    added version to css

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -2,11 +2,11 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Maurice Ibe Foundation</title>
-<link rel="stylesheet" href="{{asset('stylesheet/reset.css')}}">
-<link rel="stylesheet" href="{{asset('stylesheet/style.css')}}">
-<link rel="stylesheet" href="{{asset('stylesheet/animate.css')}}">
-<link rel="stylesheet" href="{{asset('stylesheet/chirota-icon.css')}}">
-<link rel="stylesheet" href="{{asset('stylesheet/icofont.css')}}">
+<link rel="stylesheet" href="{{asset('stylesheet/reset.css')}}?ver=1.1">
+<link rel="stylesheet" href="{{asset('stylesheet/style.css')}}?ver=1.1">
+<link rel="stylesheet" href="{{asset('stylesheet/animate.css')}}?ver=1.1">
+<link rel="stylesheet" href="{{asset('stylesheet/chirota-icon.css')}}?ver=1.1">
+<link rel="stylesheet" href="{{asset('stylesheet/icofont.css')}}?ver=1.1">
 <script src="{{ asset('js/app.js') }}" defer></script>
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
